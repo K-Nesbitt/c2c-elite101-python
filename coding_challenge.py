560f44936624fe787202f2fd77e5f09a60577617
Keatra Nesbitt <49926788+K-Nesbitt@users.noreply.github.com>
2025-02-13 21:36:43 +0000

    new lessons

---
+++ coding_challenge.py
@@ -0,0 +1,30 @@
+# determine if a string contains a digit
+# inputs - string (ie. "hello world")
+# output - True / False
+# edge cases - user puts in a list, dictionary, number
+
+#patterns - if statement (ie. if input contains digit output True),
+# patterns - for loop to go through each character in the string
+# how do we determine  - isdigit()
+
+# TO DO: WRITE PSUEDOCODE
+def contains_digit(input_string):
+    #
+
+
+    return
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
